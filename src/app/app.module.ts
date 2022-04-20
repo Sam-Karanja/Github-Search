5c0d52ae793d592533cb48112eaf666d9f29f007
Moringa School <karanjasam210@gmail.com>
2022-04-20 08:49:59 +0300

    specify declarations and import router module paths and other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,36 @@ import { UserComponent } from './components/user/user.component';
 import { ReposComponent } from './components/repos/repos.component';
 import { NoUserComponent } from './components/no-user/no-user.component';
 import { JoinPipe } from './pipes/join.pipe';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { TruncatePipe } from './pipes/truncate.pipe';
 import { NavComponent } from './components/nav/nav.component';
 import { HoverDirective } from './directives/hover.directive';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ReposComponent,
+    NoUserComponent,
+    JoinPipe,
+    TruncatePipe,
+    NavComponent,
+    HoverDirective,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'user', component: UserComponent},
+      {path: 'repos', component: ReposComponent},
+      {path: '', redirectTo:'/user',pathMatch: 'full'},
+      {path: '**', component:NoUserComponent}
+    ])
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
