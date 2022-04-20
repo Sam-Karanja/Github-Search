48d32a873cc3cd24216c7c4e10509ae86bc2c252
Moringa School <karanjasam210@gmail.com>
2022-04-20 08:53:17 +0300

    create class repo  and specified data types

---
+++ src/app/repo.ts
@@ -0,0 +1,11 @@
+export interface Repo{
+    name:string;
+    html_url:string;
+    description:string;
+    stargazers_count:number;
+    forks_count:number;
+    watcher_count:number;
+    topics:[];
+    language:string;
+    size:number;
+}
