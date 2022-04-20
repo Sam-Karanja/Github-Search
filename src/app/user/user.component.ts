fd2033d5db747bc20d34981b40d94e5aa02b5c29
Moringa School <karanjasam210@gmail.com>
2022-04-20 09:52:30 +0300

    add typescript code to get user details

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import {faStar,faCode,faEye,faSearch,faFolder,faLink} from '@fortawesome/free-solid-svg-icons';
+import { Repo } from '../../Repo';
+import { User } from '../../User';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  userDetails!: User;
+  topics!:string;
+  repos!: Repo[];
+  defaultUser!:User;
+
+  // icons
+  faEye = faEye;
+  faStar = faStar;
+  faCode = faCode;
+  faSearch = faSearch;
+  faFolder = faFolder;
+  faLink = faLink;
+
+  constructor(private _githubService: GithubService) {
+
+
   }
+  ngOnInit(){
+     this.getUserDetails('Sam-Karanja')
+  }
+
+
+
+  getUserDetails(username:string){
+    this._githubService.getUser(username).then(
+
+      (success)=>{
+        this.userDetails = this._githubService.user;
+        console.log(success)
+      },
+      (error)=>{
+        alert("User not found");
+        console.log(error)
+      }
+    );
+
+    this._githubService.getUserRepos(username).then(
+      ()=>{
+        this.repos = this._githubService.userRepos;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+   
+  }
+
+
 
 }
