cd1366bdae5c18c67c4eb5345503364ac17a2d3e
Moringa School <karanjasam210@gmail.com>
2022-04-20 09:36:23 +0300

    add constructor instances for repos comonent

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {faStar,faCode,faEye,faLink,faFolder,faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import { GithubService } from 'src/app/services/github.service';
+import { Repo } from '../../Repo';
 
 @Component({
   selector: 'app-repos',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+ 
+  repos!: Repo[];
 
-  constructor() { }
+
+  // icons
+  faEye = faEye;
+  faStar = faStar;
+  faCode = faCode;
+  faFolder = faFolder;
+  faLink = faLink;
+  faArrowLeft = faArrowLeft;
+
+  constructor(private _githubService:GithubService) { }
 
   ngOnInit(): void {
+    this.getRepos("fight")
+  }
+
+
+
+  getRepos(repoName:string){
+      this._githubService.searchRepos(repoName).then(
+        ()=>{
+             this.repos = this._githubService.repos;
+             console.log(this.repos);
+        },
+        (error)=>{
+          alert("An error occured please try again");
+        }
+      )
+
   }
 
 }
