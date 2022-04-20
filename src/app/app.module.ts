a48514ba8e87df193d154d53034a8600dea0b5bc
Moringa School <karanjasam210@gmail.com>
2022-04-20 08:39:36 +0300

    add imports in the module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { NoUserComponent } from './components/no-user/no-user.component';
+import { JoinPipe } from './pipes/join.pipe';
+import { RouterModule } from '@angular/router';
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { NavComponent } from './components/nav/nav.component';
+import { HoverDirective } from './directives/hover.directive';
 @NgModule({
   declarations: [
     AppComponent
