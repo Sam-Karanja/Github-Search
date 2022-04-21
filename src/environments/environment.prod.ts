64f56e58c1ad4dfb1beaffba51239e1951b89e44
Moringa School <karanjasam210@gmail.com>
2022-04-21 21:43:09 +0300

    add base_url and base_repo_url to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: "https://api.github.com/users",
-  base_repo_url:"https://api.github.com/search/repositories?q="
+   base_url: "https://api.github.com/users",
+    base_repo_url:"https://api.github.com/search/repositories?q="
 };
