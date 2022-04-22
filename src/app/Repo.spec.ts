5d40c5c20853afc3cfac9c192ec5d34d9f50d4c0
Moringa School <karanjasam210@gmail.com>
2022-04-22 13:18:27 +0300

    Auto-generated commit [ci skip]

--- src/app/Repo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Repo } from './Repo';
-
-describe('Users', () => {
-  it('should create an instance', () => {
-    expect(new Repo()).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-   base_url: "https://api.github.com/users",
-    base_repo_url:"https://api.github.com/search/repositories?q="
-};
